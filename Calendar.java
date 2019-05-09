eda002ba29ae874cdff27985388668f799297228
Roeland Kulk <roelandkulk@gmail.com>
2019-05-09 08:46:24 +0200

    Refactored, fixed bugs, structured files

---
+++ Calendar.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+class Calendar {
+  static ArrayList<Event> eventsList = new ArrayList<>();
+
+  static Event createEvent(String title, String date) {
+    Event eventInstance = new Event(title, date);
+    return eventInstance;
+  }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Java calendar
+
+This is an CLI app which allows users to add, edit, delete and display events in a calendar.
